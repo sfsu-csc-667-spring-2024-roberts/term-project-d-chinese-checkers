d8b49504c4ab42622766e4177cede271297ff89a
JohnnyJones <c18jonathan.gale@gmail.com>
2024-03-21 16:49:58 -0400

    add httperrors

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes/root');
+const httperrors = require('http-errors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
